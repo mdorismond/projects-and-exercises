a738556794a3a6868dc3cb7be59b4aa5fdddbbb2
Isaac Chow <isaac.chow@techelevator.com>
2022-10-03 08:43:49 -0500

    Mod1 16 pdf

---
+++ bonus-sessions/Module-1-14-OOPDemo-lecture-final/src/main/java/RobinHoodBear.java
@@ -0,0 +1,20 @@
+
+public class RobinHoodBear extends Baloo implements ISidekick{
+    private String catchphrase;
+    private Sidekick sidekick;
+
+    public RobinHoodBear(String name, int height, String furColor, String catchphrase) {
+        super(name, height, furColor, catchphrase);
+        sidekick = new Sidekick();
+    }
+
+    @Override
+    public String getCatchPhrase() {
+        return "Give to the poor";
+    }
+
+    @Override
+    public Sidekick getSideKick() {
+        return this.sidekick;
+    }
+}
