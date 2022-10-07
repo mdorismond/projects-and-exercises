d214e0c410dbc8bf5f932bfd11eb9adf949f40d4
Madalyn Dorismond <mdorismond25@gmail.com>
2022-10-07 10:55:20 -0500

    filereading

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,9 +1,48 @@
 package com.techelevator;
+import java.util.*;
+import java.io.*;
 
 public class WordSearch {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+
+System.out.println("What is the file path?");
+Scanner input = new Scanner(System.in);
+String path = input.nextLine();
+
+System.out.println("What word are you searching for?");
+String searchWord = input.nextLine();
+
+System.out.println("Should the search be case sensitive? Y/N");
+String caseInput = input.nextLine();
+
+boolean isCaseSensitive = caseInput.equals("Y");
+
+File file = new File(path);
+
+int i = 1;
+
+Scanner fileInput = new Scanner(file);
+
+	while(fileInput.hasNextLine()) {
+		String line = fileInput.nextLine();
+		if(isCaseSensitive == false){
+			if(line.toLowerCase().contains(searchWord.toLowerCase())){
+				System.out.println(i + ") "  + line);
+			}
+		} else {
+			if(line.contains(searchWord)){
+				System.out.println(i + ") "  + line);
+
+			}
+		}
+		i++;
 
 	}
 
-}
+
+	}
+	}
+
+
+
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FileSplitter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FileSplitter.java
@@ -1,9 +1,93 @@
 package com.techelevator;
+import java.util.*;
+import java.io.*;
 
 public class FileSplitter {
 
 	public static void main(String[] args) {
 
-	}
 
+
+// object for user input
+		Scanner in = new Scanner(System.in);
+
+		// declare variables
+		int flag = 0;
+		int lineNumber = 0;
+
+		// prompt user to enter a file name
+		// we can change files here as per our choice
+		System.out.println("What is the fully qualified name of the file that should be searched?");
+		String filename = in.nextLine();
+
+		// prompt user to enter a word to search
+		System.out.println("What is the search word you are looking for?");
+		String word = in.nextLine();
+
+		// prompt user for choice
+		System.out.println("Should the search be case sensitive? (Y/N)");
+		String choice = in.nextLine();
+
+		// choice is N then make word to lower case and set flag as 1
+		if(choice.equals("N"))
+		{
+			word = word.toLowerCase();
+			flag = 1;
+		}
+
+		// create an object of BufferedReader class for file reading
+		BufferedReader reader;
+		try
+		{
+			// open file
+			reader = new BufferedReader(new FileReader(filename));
+
+			// read first line
+			String line = reader.readLine();
+
+			// increase line number by 1
+			lineNumber++;
+
+			// now, loop to read all remaining lines
+			while (line != null)
+			{
+				// check if flag is 1
+				if(flag == 1)
+				{
+					// convert line to lower case
+					String temp = line.toLowerCase();
+
+					// now, check if this line contains the given word as a substring
+					if(temp.contains(word))
+					{
+						// if yes then print line number along with line
+						System.out.println(lineNumber + ")" + line);
+					}
+				}
+
+				// otherwise,
+				else
+				{
+					// directly check for word in the line
+					if(line.contains(word))
+					{
+						System.out.println(lineNumber + ")" + line);
+					}
+				}
+
+				// read next line and increase line number by 1
+				line = reader.readLine();
+				lineNumber++;
+			}
+
+			// close file
+			reader.close();
+		}
+
+		// exception handling
+		catch (IOException e)
+		{
+			System.out.println("File not found!");
+		}
+	}
 }
