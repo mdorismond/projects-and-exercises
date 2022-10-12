2ebd8d80234f1d3e21647dbcb507bc34b3974ca2
Madalyn Dorismond <mdorismond25@gmail.com>
2022-10-12 00:16:57 -0500

    database

--- module-2/01_Intro_to_Databases/lecture/lecture-queries.sql
+++ module-2/01_Intro_to_Databases/lecture/lecture-queries.sql
@@ -1,49 +1,61 @@
 -- SELECT
 -- Use a SELECT statement to return a literal string
-
+SELECT 'Hello SQL'
 
 -- Use a SELECT statement to add two numbers together (and label the result "sum")
-
+SELECT 2 + 2 AS sum
 
 
 -- SELECT ... FROM
 -- Write queries to retrieve...
 
 -- The names from all the records in the state table
-
+SELECT state_name 
+FROM state
 
 -- The names and populations of all cities
-
-
+SELECT city_name, population
+FROM city
 -- All columns from the park table
-
+Select * FROM park
 
 
 -- SELECT __ FROM __ WHERE
 -- Write queries to retrieve...
 
 -- The names of cities in California (CA)
-
+SELECT city_name
+FROM city
+WHERE state_abbreviation = 'CA'
 
 -- The names and state abbreviations of cities NOT in California
-
+SELECT city_name,state_abbreviation
+FROM city
+WHERE state_abbreviation <> 'CA'
 
 -- The names and areas of cities smaller than 25 square kilometers 
-
+SELECT city_name, area
+FROM city
+WHERE area < 25
 
 -- The names from all records in the state table that have no assigned census region
-
+SELECT state_name
+FROM state
+WHERE census_region IS null
 
 -- The names and census regions from all records in the state table that have an assigned census region
-
+SELECT state_name, census_region
+FROM state
+WHERE census_region IS NOT null
 
 
 -- WHERE with AND/OR
 -- Write queries to retrieve...
 
 -- The names, areas, and populations of cities smaller than 25 sq. km. with more than 100,000 people
-
-
+SELECT city_name, area, population		
+FROM city
+WHERE area < 25 AND population > 100000
 -- The names and census regions of all states (and territories and districts) not in the Midwest region (including those that don't have any census region)
 
 
@@ -51,7 +63,9 @@
 
 
 -- The names, areas, and populations of cities in New England -- Connecticut (CT), Maine (ME), Massachusetts (MA), New Hampshire (NH), Rhode Island (RI) and Vermont (VT)
-
+SELECT city_name, area, population
+FROM city
+WHERE state_abbreviation IN ('CT', 'ME' , 'MA' , 'NH', 'RI' , 'VT')
 
 
 -- SELECT statements involving math
@@ -74,20 +88,30 @@
 -- Write queries to retrieve...
 
 -- The names of all cities that begin with the letter "A"
-
+SELECT city_name
+FROM city
+WHERE city_name LIKE 'A%'
 
 -- The names of all cities that end with "Falls"
-
+SELECT city_name
+FROM city
+WHERE city_name LIKE '% Falls'
 
 -- The names of all cities that contain a space
-
+SELECT city_name
+FROM city
+WHERE city_name LIKE '% %'
 
 
 -- BETWEEN
 -- Write a query to retrieve the names and areas of parks of at least 100 sq. kilometers but no more than 200 sq. kilometers
-
+SELECT park_name
+FROM park
+WHERE area BETWEEN 100 and 200;
 
 
 -- DATES
 -- Write a query to retrieve the names and dates established of parks established before 1900
-
+SELECT park_name, date_established
+FROM park
+WHERE date_established < '1/1/1900'
