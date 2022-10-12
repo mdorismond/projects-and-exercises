f65d1ac6ddbfb4914e8ad06d3e037d4a0859f7fa
Madalyn Dorismond <mdorismond25@gmail.com>
2022-10-12 11:45:07 -0500

    database

--- module-2/01_Intro_to_Databases/exercise/Exercises/04_name_population_nickname_northeast.sql
+++ module-2/01_Intro_to_Databases/exercise/Exercises/04_name_population_nickname_northeast.sql
@@ -1,3 +1,4 @@
 -- 4. The name, population, and nickname of the states in the "Northeast" census region (9 rows)
 Select state_name, population, state_nickname
 from state
+where census_region = 'Northeast'
