4ff58d27325badc39e6fb1562e04433baf713c3a
Madalyn Dorismond <mdorismond25@gmail.com>
2022-10-27 13:06:34 -0500

    daoaccesshw

--- module-2/07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcEmployeeDao.java
+++ module-2/07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcEmployeeDao.java
@@ -8,6 +8,7 @@ import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.techelevator.projects.model.Employee;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 public class JdbcEmployeeDao implements EmployeeDao {
 
@@ -19,31 +20,82 @@ public class JdbcEmployeeDao implements EmployeeDao {
 	
 	@Override
 	public List<Employee> getAllEmployees() {
-		return new ArrayList<>();
+		List<Employee> allEmployees = new ArrayList<>();
+		String sqlGetAllEmployees = "SELECT employee.employee_id, department_id, first_name, last_name, birth_date, hire_date FROM employee";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetAllEmployees);
+		while (results.next()) {
+			Employee employeeResult = mapRowToEmployee(results);
+			allEmployees.add(employeeResult);
+		}
+		return allEmployees;
 	}
+
+
+
 
 	@Override
 	public List<Employee> searchEmployeesByName(String firstNameSearch, String lastNameSearch) {
-		return List.of(new Employee());
+		List<Employee> employeesByName = new ArrayList<>();
+		String sqlSearchEmployeesByName = "SELECT employee_id, department_id, first_name, last_name, birth_date, hire_date FROM employee WHERE first_name ILIKE ? AND last_name ILIKE ? ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlSearchEmployeesByName, "%" + firstNameSearch + "%", "%" + lastNameSearch + "%");
+		while(results.next()) {
+			Employee employeeName = mapRowToEmployee(results);
+			employeesByName.add(employeeName);
+		}
+		return employeesByName;
 	}
+
 
 	@Override
 	public List<Employee> getEmployeesByProjectId(int projectId) {
-		return new ArrayList<>();
+		List<Employee> employeeByProject = new ArrayList<Employee>();
+		String sqlFindEmployeesByProjectId = "SELECT employee.employee_id, department_id, first_name, last_name, birth_date, hire_date FROM employee JOIN project_employee ON employee.employee_id = project_employee.employee_id WHERE project_id = ? ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlFindEmployeesByProjectId, projectId);
+		while(results.next()) {
+			Employee employeeProjectId = mapRowToEmployee(results);
+			employeeByProject.add(employeeProjectId);
+		}
+		return employeeByProject;
 	}
 
 	@Override
 	public void addEmployeeToProject(int projectId, int employeeId) {
+		String sql = "INSERT INTO project_employee(project_id,employee_id) VALUES ( ?, ? )";
+		jdbcTemplate.update(sql,projectId,employeeId);
 	}
 
 	@Override
 	public void removeEmployeeFromProject(int projectId, int employeeId) {
-	}
+		String sql = "DELETE FROM project_employee WHERE employee_id = ? AND project_id = ?";
+			jdbcTemplate.update(sql, employeeId,projectId);
+		}
+
 
 	@Override
 	public List<Employee> getEmployeesWithoutProjects() {
-		return new ArrayList<>();
+		List<Employee> employeesWithoutProjects = new ArrayList<Employee>();
+		String sqlGetEmployeesWithoutProjects = "SELECT employee_id, department_id, first_name, last_name, birth_date, hire_date FROM employee WHERE employee_id NOT IN (SELECT DISTINCT employee_id FROM project_employee) ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetEmployeesWithoutProjects);
+		while(results.next()) {
+			Employee benchEmployee = mapRowToEmployee(results);
+			employeesWithoutProjects.add(benchEmployee);
+		}
+		return employeesWithoutProjects;
 	}
 
 
-}
+	private Employee mapRowToEmployee(SqlRowSet results) {
+		Employee employee;
+		employee = new Employee();
+		employee.setId(results.getInt("employee_id"));
+		employee.setDepartmentId(results.getInt("department_id"));
+		employee.setFirstName(results.getString("first_name"));
+		employee.setLastName(results.getString("last_name"));
+		employee.setBirthDate(results.getDate("birth_date").toLocalDate());
+		employee.setHireDate(results.getDate("hire_date").toLocalDate());
+		return employee;
+	}
+	}
+
+
+
--- module-2/07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcProjectDao.java
+++ module-2/07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcProjectDao.java
@@ -20,23 +20,57 @@ public class JdbcProjectDao implements ProjectDao {
 
 	@Override
 	public Project getProject(int projectId) {
-		return new Project(0, "Not Implemented Yet", null, null);
+		Project project = null;
+		String sqlGetProject = "SELECT project_id, name, from_date, to_date FROM project WHERE project_id = ?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetProject, projectId);
+		if (results.next()) {
+			project = MapRowToProject(results);
+		}
+		return project;
 	}
 
 	@Override
 	public List<Project> getAllProjects() {
-		return new ArrayList<>();
+		List<Project> allProjects = new ArrayList<>();
+		String sqlGetAllProjects = "SELECT project_id, name, from_date, to_date FROM project";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetAllProjects);
+		while (results.next()) {
+			Project project = MapRowToProject(results);
+			allProjects.add(project);
+		}
+
+		return allProjects;
 	}
+
 
 	@Override
 	public Project createProject(Project newProject) {
-		return null;
+		String sql = "INSERT INTO project( name, from_date, to_date) VALUES (?, ?, ?) RETURNING project_id";
+		int rowsInserted = jdbcTemplate.queryForObject(sql, Integer.class, newProject.getName(), newProject.getFromDate(), newProject.getToDate());
+		return getProject(rowsInserted);
 	}
 
 	@Override
 	public void deleteProject(int projectId) {
+		String sql = "DELETE FROM project_employee WHERE project_id = ?";
+		jdbcTemplate.update(sql, projectId);
+		sql = "DELETE FROM project WHERE project_id = ?";
+
+		jdbcTemplate.update(sql, projectId);
+	}
+
+	private Project MapRowToProject(SqlRowSet rowSet) {
+		Project project = new Project();
+		project.setId(rowSet.getInt("project_id"));
+		project.setName(rowSet.getString("name"));
+		if (rowSet.getDate("from_date") != null)
+			project.setFromDate(rowSet.getDate("from_date").toLocalDate());
+		if (rowSet.getDate("to_date") != null)
+			project.setToDate(rowSet.getDate("to_date").toLocalDate());
+		return project;
 
 	}
+}
 	
 
-}
+
