e9be50362d1e914ebcc522af5ec3d870286f0b73
Madalyn Dorismond <mdorismond25@gmail.com>
2022-11-08 11:27:41 -0600

    serversideAPI

--- module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,7 +2,13 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+
 
 @RestController
 @RequestMapping("/auctions")
@@ -13,5 +19,26 @@ public class AuctionController {
     public AuctionController() {
         this.dao = new MemoryAuctionDao();
     }
+    @RequestMapping (method = RequestMethod.GET)
+public List<Auction> list() {
+
+        return dao.list();
+    }
+    @RequestMapping (path = "/{id}", method = RequestMethod.GET)
+    public Auction get( @PathVariable int id) {
+      return dao.get(id);
+
+    }
+    @RequestMapping (method = RequestMethod.POST)
+    public Auction create(@RequestBody Auction auction) {
+        return dao.create(auction);
+
+    }
+@RequestMapping (path = "/filter", method = RequestMethod.GET)
+    List<Auction> searchByTitle(@RequestParam String title_like){
+
+       return dao.list();
+}
+
 
 }
